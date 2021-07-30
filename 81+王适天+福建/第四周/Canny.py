7dc58811a4e7adec8345a7a94e5fc01a5eb30b8a
Genius_Tian <wst452871153@gmail.com>
2021-07-30 08:37:04 +0800

    第四周作业Canny算子老师代码理解2

--- 81+王适天+福建/第四周/Canny.py
+++ 81+王适天+福建/第四周/Canny.py
@@ -28,3 +28,25 @@ def Canny(img, sigma, threshold1, threshold2):
     for dst_h in range(h):
         for dst_w in range(w):
             img_new[dst_h, dst_w] = np.sum(img_pad[dst_h:dst_h + dim, dst_w:dst_w + dim] * Gaussian_filter)
+
+    #     使用sobel算子检测边缘
+    sobel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    sobel_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+    img_gradient_x = np.zeros(img_new.shape)
+    img_gradient_y = np.zeros(img_new.shape)
+    img_gradient = np.zeros(img_new.shape)
+    img_pad = np.pad(img_new, ((1, 1), (1, 1)), 'constant')
+    for i in range(h):
+        for j in range(w):
+            img_gradient_x[i, j] = np.sum(img_pad[i + 3, j + 3] * sobel_x)
+            img_gradient_y[i, j] = np.sum(img_pad[i + 3, j + 3] * sobel_y)
+            img_gradient[i, j] = np.sqrt(img_gradient_x[i, j] ** 2 + img_gradient_y ** 2)
+
+
+if __name__ == '__main__':
+    a = np.array([[255, 255], [255, 255]])
+    b = np.array([[1, 2], [3, 4]])
+    zeros = np.zeros(a.shape)
+    zeros += np.sum(a * b)
+    print(zeros.astype(np.uint8))
+    print(a[:1, :])
